a7d0f693741ed1daca3064df806d97638fff481d
Bjørn Wikkeling <bjorn@wikkeling.com>
2026-01-18 11:45:56 +0100

    cli: Add install marker to all installation methods
    Create marker file in all installers for analytics tracking:
    - install.cmd: creates %LOCALAPPDATA%\klaas\.installed
    - Homebrew formula: creates ~/.local/share/klaas/.installed
    - Scoop manifest: creates %LOCALAPPDATA%\klaas\.installed

--- Formula/klaas.rb
+++ Formula/klaas.rb
@@ -36,6 +36,15 @@ class Klaas < Formula
     bin.install "klaas"
   end
 
+  def post_install
+    # Create install marker for analytics (tracks install event on first run)
+    # Use XDG_DATA_HOME or default to ~/.local/share
+    data_home = ENV["XDG_DATA_HOME"] || File.join(Dir.home, ".local", "share")
+    data_dir = Pathname.new(data_home)/"klaas"
+    data_dir.mkpath
+    (data_dir/".installed").write(version.to_s)
+  end
+
   test do
     assert_match version.to_s, shell_output("#{bin}/klaas --version")
   end
